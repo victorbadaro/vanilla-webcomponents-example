a7382b07a10f0ff4a81015416921f0291c27cd37
Victor Badaró <victor.badaro92@gmail.com>
2026-08-08 14:15:42 -0300

    chore: add some basic styles for the custom button

--- custom-button.js
+++ custom-button.js
@@ -5,6 +5,26 @@ class CustomButton extends HTMLElement {
     const shadowDOM = this.attachShadow({ mode: 'open' });
 
     shadowDOM.innerHTML = `
+      <style>
+        button {
+          padding: 8px 16px;
+          border: none;
+          border-radius: 4px;
+          cursor: pointer;
+          background-color: #9ae600;
+          color: #192e03;
+          transition: background-color 200ms, transform 200ms;
+        }
+
+        button:hover {
+          background-color: #7ccf00;
+        }
+
+        button:active {
+          transform: scale(0.98);
+        }
+      </style>
+
       <button type="button">This is a Custom Button!</button>
     `;
   }
